51423401a5e0b7fcb0c54f7a0f205c4dda986da6
Safi <syedsafi3414@gmail.com>
2026-04-10 23:00:11 +0500

    feat: partial user routes set up and fix API endpoint path for auth routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import connectDB from './lib/db.js';
 import cookieParser from 'cookie-parser';
 
@@ -12,12 +13,13 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB();
 });
 
-app.use("/api/auth", authRoutes);
+
 
