e4071225ecf60be208bfa61d12abef5d419f6f65
Safi <syedsafi3414@gmail.com>
2026-04-26 15:38:06 +0500

    integrate react-query and axios for API calls, add new pages and update routing

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,17 @@ import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import connectDB from './lib/db.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
+import NotificationPage from './pages/NotificationPage'
+import OnboardingPage from './pages/OnboardingPage'
+import CallPage from './pages/CallPage'
+import ChatPage from './pages/ChatPage'
+import { Toaster } from 'react-hot-toast'
+import { useQuery } from '@tanstack/react-query'
+import { axiosInstance } from './lib/axios'
 
 const App = () => {
-  return (
+ const {data, isLoading, error} = useQuery({ queryKey: ['todos'],
+   queryFn: async () => {
+    const res= await axiosInstance.get('/auth/me')
+    return res.data
+   },
+   retry: false
+   }) 
+ 
+ return (
     <div>
-      
+      <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/notification" element={<NotificationPage />} />
+          <Route path="/onboarding" element={<OnboardingPage />} />
+          <Route path="/call" element={<CallPage />} />
+          <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+      <Toaster/>
     </div>
   )
 }
